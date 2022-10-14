2057b4e9496bdcc9c6d609ff7146dcc7de192cdb
GolubevNY <vvertoff85@gmail.com>
2022-10-14 22:09:52 +0300

    ssh test connection

--- terraform/stage/vm.tf
+++ terraform/stage/vm.tf
@@ -10,8 +10,9 @@ resource "yandex_compute_instance" "revproxy" {
 
   boot_disk {
     initialize_params {
-      #image_id = "fd84mnpg35f7s7b0f5lg" // nat-instance-ubuntu-1804-lts
-      image_id = "fd8v7ru46kt3s4o5f0uo"
+      image_id = "fd84mnpg35f7s7b0f5lg" // nat-instance-ubuntu-1804-lts
+      #image_id = "fd8v7ru46kt3s4o5f0uo"
+      #image_id = "fd8r7bcu7425iemee64g" //with ssh key
       type     = "network-hdd"
       size     = "10"
     }
@@ -25,8 +26,9 @@ resource "yandex_compute_instance" "revproxy" {
   }
 
   metadata = {
-#    serial-port-enable: 1
+    serial-port-enable: 1
     ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+#    user-data = "${file("meta.txt")}"    
   }
 }    
 resource "yandex_compute_instance" "db01" {
@@ -42,6 +44,7 @@ resource "yandex_compute_instance" "db01" {
   boot_disk {
     initialize_params {
       image_id = "fd8h34rub6pjpg4qgi8j" // ubuntu-1804-lts
+      #image_id = "fd8hfonufqct6rm05bmv" // with ssh key
       type     = "network-hdd"
       size     = "10"
     }
@@ -49,11 +52,13 @@ resource "yandex_compute_instance" "db01" {
 
   network_interface {
     subnet_id   = yandex_vpc_subnet.subnet[1].id
+
     ipv6        = false
   }
 
   metadata = {
     serial-port-enable: 1
     ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+#    user-data = "${file("meta.txt")}"    
   }
 }
--- terraform/stage/vpc.tf
+++ terraform/stage/vpc.tf
@@ -2,14 +2,14 @@ resource "yandex_vpc_network" "net" {
   name                    = "net"
 }
 
-resource "yandex_vpc_route_table" "nat-instance-route" {
-  name                    = "nat-instance-route"
-  network_id              = yandex_vpc_network.net.id
-  static_route {
-    destination_prefix    = "0.0.0.0/0"
-    next_hop_address      = var.revproxy_ip
-  }
-}
+#resource "yandex_vpc_route_table" "nat-instance-route" {
+ # name                    = "nat-instance-route"
+  #network_id              = yandex_vpc_network.net.id
+  #static_route {
+   # destination_prefix    = "0.0.0.0/0"
+    #next_hop_address      = var.revproxy_ip
+  #}
+#}
 
 resource "yandex_vpc_subnet" "subnet" {
   count                   = 3
@@ -17,5 +17,5 @@ resource "yandex_vpc_subnet" "subnet" {
   zone                    = var.zones[count.index]
   network_id              = yandex_vpc_network.net.id
   v4_cidr_blocks          = [var.cidr[count.index]]
-  route_table_id          = yandex_vpc_route_table.nat-instance-route.id
+  #route_table_id          = yandex_vpc_route_table.nat-instance-route.id
 }
